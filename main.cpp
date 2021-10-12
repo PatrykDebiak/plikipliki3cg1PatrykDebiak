394652ac7bbb42b66f62de8642432c4b476410db
PatrykDebiak <91215263+PatrykDebiak@users.noreply.github.com>
2021-10-12 14:30:12 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <fstream>
+
+
+
+using namespace std;
+
+int main()
+{
+    fstream plik;
+    int x, y;
+    ofstream myfile;
+    plik.open("wsp.txt", ios::in);
+    myfile.open("wynik.txt", ios::app);
+    if (plik.good() == true)
+    {
+        while (!plik.eof())
+        {
+            plik >> x >> y;
+            if (y > 0 && x < 0) {
+                myfile << x << " " << y << "\tPierwsza cwiartka\n";
+            }
+
+
+            else if (y > 0 && x > 0) {
+                myfile << x << " " << y << "\tDruga cwiartka\n";
+            }
+
+
+            else if (y < 0 && x < 0) {
+                myfile << x << " " << y << "\tTrzecia cwiartka\n";
+            }
+
+
+            else if (y < 0 && x > 0) {
+                myfile << x << " " << y << "\tCzwarta cwiartka\n";
+            }
+
+
+
+        }
+        myfile.close();
+        plik.close();
+       
+
+    }
+}
